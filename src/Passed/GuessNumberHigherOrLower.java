b4fd06a13cccf0cf524d8edf6aa60bcaa0a94341
zhang.lijuan78 <zhang.lijuan78@gmail.com>
2016-07-25 21:12:32 -0700

    1

---
+++ src/Passed/GuessNumberHigherOrLower.java
@@ -0,0 +1,17 @@
+package Passed;
+
+public class GuessNumberHigherOrLower {
+	public int guessNumber(int n) {
+        int low = 1, high = n;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (guess(mid) == 0) return mid;
+            if (guess(mid) == 1) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
